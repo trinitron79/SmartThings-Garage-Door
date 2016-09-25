d6c5bcc439b9dd93f6ea798913240e2947b3faa9
Joshua Newton <trinitron79@gmail.com>
2016-09-25 13:52:12 -0600

    Current as of 9-25-2016

--- smartapps/smartthings/automated-garage-door-open-close.src/automated-garage-door-open-close.groovy
+++ smartapps/smartthings/automated-garage-door-open-close.src/automated-garage-door-open-close.groovy
@@ -31,7 +31,7 @@
 
 definition(
     name: "Automated Garage Door (Open / Close)",
-    namespace: "smartthings",
+    namespace: "SmartThings",
     author: "SmartThings",
     description: "Monitors arrival and departure of car(s) and 1) opens door when car arrives, 2) closes door after car has departed (for N minutes), 3) opens door when car door motion is detected, 4) closes door when door was opened due to arrival and interior door is closed. Modified the orginial version to have the garage door automatically close and send notifications.  Also changed to allow temporary override door open checks.",
     category: "Convenience",
